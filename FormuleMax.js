84aac47a76b4ff9de082bdb91d6469ea66e95b26
kimseongeun1212 <62641359+kimseongeun1212@users.noreply.github.com>
2021-07-11 23:37:42 +0900

    0711

--- FormuleMax.js
+++ FormuleMax.js
@@ -1,9 +1,43 @@
 
+const calculate = (arr, operator) => {
+    arr = arr.map(v => parseInt(v, 10));
+    if (operator === '*') {
+        return arr.reduce((acc, cur) => acc * cur);
+    }
+    else if (operator === '+') {
+        return arr.reduce((acc, cur) => acc + cur);
+    }
+    else {
+        return arr.reduce((acc, cur) => acc - cur);
+    }
+};
 
-function solution(expression) {
+const solution = expression => {
+
+    const cycle = [
+        '*+-',
+        '*-+',
+        '+*-',
+        '+-*',
+        '-*+',
+        '-+*',
+    ];
+
+    let max = 0;
+    for (let i = 0; i < 6; i += 1) {
+        const op = cycle[i];
+        const value = calculate(expression.split(op[2]).map(v => calculate(v.split(op[1]).map(k => calculate(k.split(op[0]), op[0])), op[1])), op[2]);
+        if (Math.abs(value) > max) max = Math.abs(value);
+    }
+    return max;
+};
+
+console.log(solution("100-200*300-500+20"));
+
+/*function solution(expression) {
     var answer = 0;
     var separators = ['+', '-', '*'];
-    var str=expression.split(new RegExp('[-+*]')); //수식 기호 기준으로 분리하기
+    var nums=expression.split(new RegExp('[-+*]')); //수식 기호 기준으로 분리하기
 
 
 
@@ -17,4 +51,4 @@ function test(){
     console.log(solution(expression));
 }
 
-test();
+test();*/
